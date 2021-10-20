d3f5793fe78cabe2335f6a7e14fd4461e6373a6e
kvammee <elliott.kvamme@gmail.com>
2021-10-19 19:58:29 -0500

    Added question for title

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-// TODO: Include packages needed for this application
+var inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter a Project title: ",
+        validate: answer => {
+            if (answer) {
+                return true;
+            } else {
+                console.log("Please enter a Project name!");
+                return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
